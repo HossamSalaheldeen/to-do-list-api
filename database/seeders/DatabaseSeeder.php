177b953bc43e3462ea08f86cbabac4466efc865c
HossamSalaheldeen <hossam.salah1995@gmail.com>
2022-06-20 22:45:22 +0200

    make task resource route
    task model
    
    task migration
    
    task factory
    
    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(3)->create()->each(function ($user){
+             Task::factory(3)->create(['user_id' => $user->id]);
+         });
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
